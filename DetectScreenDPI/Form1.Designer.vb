c8478184679d4b74fd5fb7dfcc2d8edd1a298bd0
Mawi <ma_wi@live.com>
2020-02-26 14:15:33 +0800

    Add project files.

---
+++ DetectScreenDPI/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.t1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        't1
+        '
+        Me.t1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.t1.Location = New System.Drawing.Point(12, 12)
+        Me.t1.Multiline = True
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(523, 210)
+        Me.t1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(172, 237)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(144.0!, 144.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(546, 272)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.t1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents t1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ DetectScreenDPI/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckDPI()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim g As Graphics = Me.CreateGraphics()
+        Dim t As String = "ScreenWidth:" & Screen.PrimaryScreen.Bounds.Width & " ScreenHeight:" & Screen.PrimaryScreen.Bounds.Height & vbCrLf & " DpiX:" & g.DpiX & " DpiY:" & g.DpiY
+        t1.Text = t
+        g.Dispose()
+        g = Nothing
+    End Sub
+
+    Public Sub CheckDPI()
+        Dim screenW As Integer = 0
+        Dim screenH As Integer = 0
+        Dim DpiX As Integer = 0
+        Dim DpiY As Integer = 0
+        Try
+            Using g As Graphics = Me.CreateGraphics()
+                screenW = Screen.PrimaryScreen.Bounds.Width
+                screenH = Screen.PrimaryScreen.Bounds.Height
+                DpiX = g.DpiX
+                DpiY = g.DpiY
+            End Using
+
+            If DpiX > 96 Then
+                If MsgBox($"Your screen resolution has high DPI: ({DpiX}x{DpiY})." & vbNewLine &
+                       "Some controls may not scale or display correctly." & vbNewLine & vbNewLine &
+                       "Do you want to continue loading?", vbApplicationModal + vbExclamation + vbYesNo, "Warning") = vbNo Then
+                    End
+                End If
+            End If
+        Catch ex As Exception
+            Debug.Print(ex.Message)
+        End Try
+    End Sub
+End Class
